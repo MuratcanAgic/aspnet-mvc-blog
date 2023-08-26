c90d618655e8184bd38f36715ff4218b696f50d2
Muratcan Agiç <muratcanagic@hotmail.com>
2023-08-26 20:15:42 +0300

    Gorev9 has been completed!

--- AspNetMvcBlog/App.Web.Mvc/Program.cs
+++ AspNetMvcBlog/App.Web.Mvc/Program.cs
@@ -1,7 +1,12 @@
+using App.Web.Mvc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConStr")));
 
 var app = builder.Build();
 
@@ -25,3 +30,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+    

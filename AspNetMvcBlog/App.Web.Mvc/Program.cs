d8367cffcc41605c18fb6a6ed53a7df844f3ca39
Muratcan Agiç <muratcanagic@hotmail.com>
2023-10-02 15:09:58 +0300

    Gorev14 has been completed!

--- AspNetMvcBlog/App.Web.Mvc/Program.cs
+++ AspNetMvcBlog/App.Web.Mvc/Program.cs
@@ -1,3 +1,4 @@
+using App.Business;
 using App.Business.Services;
 using App.Business.Services.Abstract;
 using App.Persistence.Data;
@@ -16,14 +17,16 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ISettingService, SettingService>();
 builder.Services.AddScoped<IPageService, PageService>();
 
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -34,7 +37,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-		name: "default",
-		pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
